42f146a506ec44581fdc7164dad8b6426f308ea3
Prabhat kumar <pyadav1137@gmail.com>
2023-01-07 17:14:20 +0530

    2nd commit

---
+++ diff.java
@@ -0,0 +1,9 @@
+public class diff{
+    public static void main(String[] args) {
+        int a,b;
+        a=2;
+        b=5;
+        int sum=a+b;
+        System.out.println("sum is :"+sum);
+    }
+}
